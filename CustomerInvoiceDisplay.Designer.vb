9899a37c4d7f3cf5c69dfe946655869a283f231c
Aziz <azizahmed.dev@gmail.com>
2023-07-18 23:40:35 -0400

    Add project files.

---
+++ CustomerInvoiceDisplay.Designer.vb
@@ -0,0 +1,345 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CustomerInvoiceDisplay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim NameLabel As System.Windows.Forms.Label
+        Dim AddressLabel As System.Windows.Forms.Label
+        Dim CityLabel As System.Windows.Forms.Label
+        Dim DataGridViewCellStyle11 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle12 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle13 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle14 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle15 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.CustomersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MMABooksDataSet = New ahmed_exc_15_2.MMABooksDataSet()
+        Me.AddressTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipCodeTextBox = New System.Windows.Forms.TextBox()
+        Me.InvoicesBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.InvoicesDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FillByCustomerIDToolStrip = New System.Windows.Forms.ToolStrip()
+        Me.CustomerIDToolStripLabel = New System.Windows.Forms.ToolStripLabel()
+        Me.txtCustomerID = New System.Windows.Forms.ToolStripTextBox()
+        Me.FillByCustomerIDToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.CustomersTableAdapter = New ahmed_exc_15_2.MMABooksDataSetTableAdapters.CustomersTableAdapter()
+        Me.TableAdapterManager = New ahmed_exc_15_2.MMABooksDataSetTableAdapters.TableAdapterManager()
+        Me.InvoicesTableAdapter = New ahmed_exc_15_2.MMABooksDataSetTableAdapters.InvoicesTableAdapter()
+        Me.InvoicesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        NameLabel = New System.Windows.Forms.Label()
+        AddressLabel = New System.Windows.Forms.Label()
+        CityLabel = New System.Windows.Forms.Label()
+        CType(Me.CustomersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MMABooksDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.InvoicesBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.InvoicesDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.FillByCustomerIDToolStrip.SuspendLayout()
+        CType(Me.InvoicesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NameLabel
+        '
+        NameLabel.AutoSize = True
+        NameLabel.Location = New System.Drawing.Point(18, 49)
+        NameLabel.Name = "NameLabel"
+        NameLabel.Size = New System.Drawing.Size(49, 17)
+        NameLabel.TabIndex = 3
+        NameLabel.Text = "Name:"
+        '
+        'AddressLabel
+        '
+        AddressLabel.AutoSize = True
+        AddressLabel.Location = New System.Drawing.Point(18, 77)
+        AddressLabel.Name = "AddressLabel"
+        AddressLabel.Size = New System.Drawing.Size(64, 17)
+        AddressLabel.TabIndex = 5
+        AddressLabel.Text = "Address:"
+        '
+        'CityLabel
+        '
+        CityLabel.AutoSize = True
+        CityLabel.Location = New System.Drawing.Point(18, 105)
+        CityLabel.Name = "CityLabel"
+        CityLabel.Size = New System.Drawing.Size(104, 17)
+        CityLabel.TabIndex = 7
+        CityLabel.Text = "City, State, Zip:"
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "Name", True))
+        Me.NameTextBox.Location = New System.Drawing.Point(132, 46)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.ReadOnly = True
+        Me.NameTextBox.Size = New System.Drawing.Size(296, 22)
+        Me.NameTextBox.TabIndex = 1
+        '
+        'CustomersBindingSource
+        '
+        Me.CustomersBindingSource.DataMember = "Customers"
+        Me.CustomersBindingSource.DataSource = Me.MMABooksDataSet
+        '
+        'MMABooksDataSet
+        '
+        Me.MMABooksDataSet.DataSetName = "MMABooksDataSet"
+        Me.MMABooksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'AddressTextBox
+        '
+        Me.AddressTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "Address", True))
+        Me.AddressTextBox.Location = New System.Drawing.Point(132, 74)
+        Me.AddressTextBox.Name = "AddressTextBox"
+        Me.AddressTextBox.ReadOnly = True
+        Me.AddressTextBox.Size = New System.Drawing.Size(296, 22)
+        Me.AddressTextBox.TabIndex = 6
+        '
+        'CityTextBox
+        '
+        Me.CityTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "City", True))
+        Me.CityTextBox.Location = New System.Drawing.Point(132, 102)
+        Me.CityTextBox.Name = "CityTextBox"
+        Me.CityTextBox.ReadOnly = True
+        Me.CityTextBox.Size = New System.Drawing.Size(144, 22)
+        Me.CityTextBox.TabIndex = 8
+        '
+        'StateTextBox
+        '
+        Me.StateTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "State", True))
+        Me.StateTextBox.Location = New System.Drawing.Point(282, 102)
+        Me.StateTextBox.Name = "StateTextBox"
+        Me.StateTextBox.ReadOnly = True
+        Me.StateTextBox.Size = New System.Drawing.Size(40, 22)
+        Me.StateTextBox.TabIndex = 10
+        '
+        'ZipCodeTextBox
+        '
+        Me.ZipCodeTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CustomersBindingSource, "ZipCode", True))
+        Me.ZipCodeTextBox.Location = New System.Drawing.Point(328, 102)
+        Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
+        Me.ZipCodeTextBox.ReadOnly = True
+        Me.ZipCodeTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.ZipCodeTextBox.TabIndex = 12
+        '
+        'InvoicesBindingSource1
+        '
+        Me.InvoicesBindingSource1.DataMember = "FK_Invoices_Customers"
+        Me.InvoicesBindingSource1.DataSource = Me.CustomersBindingSource
+        '
+        'InvoicesDataGridView
+        '
+        Me.InvoicesDataGridView.AllowUserToAddRows = False
+        Me.InvoicesDataGridView.AllowUserToDeleteRows = False
+        Me.InvoicesDataGridView.AutoGenerateColumns = False
+        Me.InvoicesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.InvoicesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn5, Me.DataGridViewTextBoxColumn6, Me.DataGridViewTextBoxColumn7})
+        Me.InvoicesDataGridView.DataSource = Me.InvoicesBindingSource1
+        Me.InvoicesDataGridView.Location = New System.Drawing.Point(15, 157)
+        Me.InvoicesDataGridView.Name = "InvoicesDataGridView"
+        Me.InvoicesDataGridView.ReadOnly = True
+        Me.InvoicesDataGridView.RowHeadersWidth = 51
+        Me.InvoicesDataGridView.RowTemplate.Height = 24
+        Me.InvoicesDataGridView.Size = New System.Drawing.Size(891, 308)
+        Me.InvoicesDataGridView.TabIndex = 12
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "InvoiceID"
+        DataGridViewCellStyle11.Format = "d"
+        DataGridViewCellStyle11.NullValue = Nothing
+        Me.DataGridViewTextBoxColumn1.DefaultCellStyle = DataGridViewCellStyle11
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Invoice ID"
+        Me.DataGridViewTextBoxColumn1.MinimumWidth = 6
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        Me.DataGridViewTextBoxColumn1.Width = 80
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "InvoiceDate"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Invoice Date"
+        Me.DataGridViewTextBoxColumn3.MinimumWidth = 6
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.ReadOnly = True
+        Me.DataGridViewTextBoxColumn3.Width = 125
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "ProductTotal"
+        DataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle12.Format = "C2"
+        DataGridViewCellStyle12.NullValue = Nothing
+        Me.DataGridViewTextBoxColumn4.DefaultCellStyle = DataGridViewCellStyle12
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Product Total"
+        Me.DataGridViewTextBoxColumn4.MinimumWidth = 6
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.ReadOnly = True
+        Me.DataGridViewTextBoxColumn4.Width = 125
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "SalesTax"
+        DataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle13.Format = "C2"
+        DataGridViewCellStyle13.NullValue = Nothing
+        Me.DataGridViewTextBoxColumn5.DefaultCellStyle = DataGridViewCellStyle13
+        Me.DataGridViewTextBoxColumn5.HeaderText = "Sales Tax"
+        Me.DataGridViewTextBoxColumn5.MinimumWidth = 6
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        Me.DataGridViewTextBoxColumn5.ReadOnly = True
+        Me.DataGridViewTextBoxColumn5.Width = 80
+        '
+        'DataGridViewTextBoxColumn6
+        '
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "Shipping"
+        DataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle14.Format = "C2"
+        DataGridViewCellStyle14.NullValue = Nothing
+        Me.DataGridViewTextBoxColumn6.DefaultCellStyle = DataGridViewCellStyle14
+        Me.DataGridViewTextBoxColumn6.HeaderText = "Shipping"
+        Me.DataGridViewTextBoxColumn6.MinimumWidth = 6
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
+        Me.DataGridViewTextBoxColumn6.ReadOnly = True
+        Me.DataGridViewTextBoxColumn6.Width = 80
+        '
+        'DataGridViewTextBoxColumn7
+        '
+        Me.DataGridViewTextBoxColumn7.DataPropertyName = "InvoiceTotal"
+        DataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight
+        DataGridViewCellStyle15.Format = "C2"
+        DataGridViewCellStyle15.NullValue = Nothing
+        Me.DataGridViewTextBoxColumn7.DefaultCellStyle = DataGridViewCellStyle15
+        Me.DataGridViewTextBoxColumn7.HeaderText = "Invoice Total"
+        Me.DataGridViewTextBoxColumn7.MinimumWidth = 6
+        Me.DataGridViewTextBoxColumn7.Name = "DataGridViewTextBoxColumn7"
+        Me.DataGridViewTextBoxColumn7.ReadOnly = True
+        Me.DataGridViewTextBoxColumn7.Width = 125
+        '
+        'FillByCustomerIDToolStrip
+        '
+        Me.FillByCustomerIDToolStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.FillByCustomerIDToolStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CustomerIDToolStripLabel, Me.txtCustomerID, Me.FillByCustomerIDToolStripButton})
+        Me.FillByCustomerIDToolStrip.Location = New System.Drawing.Point(0, 0)
+        Me.FillByCustomerIDToolStrip.Name = "FillByCustomerIDToolStrip"
+        Me.FillByCustomerIDToolStrip.Size = New System.Drawing.Size(925, 27)
+        Me.FillByCustomerIDToolStrip.TabIndex = 13
+        Me.FillByCustomerIDToolStrip.Text = "FillByCustomerIDToolStrip"
+        '
+        'CustomerIDToolStripLabel
+        '
+        Me.CustomerIDToolStripLabel.Name = "CustomerIDToolStripLabel"
+        Me.CustomerIDToolStripLabel.Size = New System.Drawing.Size(94, 24)
+        Me.CustomerIDToolStripLabel.Text = "Customer ID:"
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(100, 27)
+        '
+        'FillByCustomerIDToolStripButton
+        '
+        Me.FillByCustomerIDToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.FillByCustomerIDToolStripButton.Name = "FillByCustomerIDToolStripButton"
+        Me.FillByCustomerIDToolStripButton.Size = New System.Drawing.Size(103, 24)
+        Me.FillByCustomerIDToolStripButton.Text = "Get Customer"
+        '
+        'CustomersTableAdapter
+        '
+        Me.CustomersTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CustomersTableAdapter = Me.CustomersTableAdapter
+        Me.TableAdapterManager.InvoicesTableAdapter = Me.InvoicesTableAdapter
+        Me.TableAdapterManager.UpdateOrder = ahmed_exc_15_2.MMABooksDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'InvoicesTableAdapter
+        '
+        Me.InvoicesTableAdapter.ClearBeforeFill = True
+        '
+        'InvoicesBindingSource
+        '
+        Me.InvoicesBindingSource.DataMember = "Invoices"
+        Me.InvoicesBindingSource.DataSource = Me.MMABooksDataSet
+        '
+        'CustomerInvoiceDisplay
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(925, 484)
+        Me.Controls.Add(Me.FillByCustomerIDToolStrip)
+        Me.Controls.Add(Me.InvoicesDataGridView)
+        Me.Controls.Add(NameLabel)
+        Me.Controls.Add(Me.NameTextBox)
+        Me.Controls.Add(AddressLabel)
+        Me.Controls.Add(Me.AddressTextBox)
+        Me.Controls.Add(CityLabel)
+        Me.Controls.Add(Me.CityTextBox)
+        Me.Controls.Add(Me.StateTextBox)
+        Me.Controls.Add(Me.ZipCodeTextBox)
+        Me.Name = "CustomerInvoiceDisplay"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Customer Invoice Display"
+        CType(Me.CustomersBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MMABooksDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.InvoicesBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.InvoicesDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.FillByCustomerIDToolStrip.ResumeLayout(False)
+        Me.FillByCustomerIDToolStrip.PerformLayout()
+        CType(Me.InvoicesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MMABooksDataSet As MMABooksDataSet
+    Friend WithEvents CustomersBindingSource As BindingSource
+    Friend WithEvents CustomersTableAdapter As MMABooksDataSetTableAdapters.CustomersTableAdapter
+    Friend WithEvents TableAdapterManager As MMABooksDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents InvoicesTableAdapter As MMABooksDataSetTableAdapters.InvoicesTableAdapter
+    Friend WithEvents NameTextBox As TextBox
+    Friend WithEvents AddressTextBox As TextBox
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents ZipCodeTextBox As TextBox
+    Friend WithEvents InvoicesBindingSource As BindingSource
+    Friend WithEvents InvoicesBindingSource1 As BindingSource
+    Friend WithEvents InvoicesDataGridView As DataGridView
+    Friend WithEvents FillByCustomerIDToolStrip As ToolStrip
+    Friend WithEvents CustomerIDToolStripLabel As ToolStripLabel
+    Friend WithEvents txtCustomerID As ToolStripTextBox
+    Friend WithEvents FillByCustomerIDToolStripButton As ToolStripButton
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn7 As DataGridViewTextBoxColumn
+End Class
---
+++ CustomerInvoiceDisplay.vb
@@ -0,0 +1,18 @@
+﻿Public Class CustomerInvoiceDisplay
+
+    Private Sub CustomerInvoiceDisplay_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ActiveControl = txtCustomerID.Control
+
+    End Sub
+
+    Private Sub FillByCustomerIDToolStripButton_Click(sender As Object, e As EventArgs) Handles FillByCustomerIDToolStripButton.Click
+        Try
+            Me.CustomersTableAdapter.FillByCustomerID(Me.MMABooksDataSet.Customers, CType(txtCustomerID.Text, Integer))
+            Me.InvoicesTableAdapter.FillByCustomerID(Me.MMABooksDataSet.Invoices, CType(txtCustomerID.Text, Integer))
+        Catch ex As System.Exception
+            System.Windows.Forms.MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+End Class
